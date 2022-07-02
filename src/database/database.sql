3b4c19390555075c8fefde2864b7bb99768b6ca3
orumurin <orumurin@gmail.com>
2022-07-02 08:14:32 +0300

    add users and refresh_tokens table

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,1 +1,16 @@
--- FIRST STEP DATABASE !
+-- Primary database structure
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR (30) NOT NULL,
+    password TEXT NOT NULL,
+    token TEXT NOT NULL,
+    refresh_token_id BIGINT NOT NULL,
+    updated_at date NOT NULL,
+    created_at date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS refresh_token (
+    refresh_token_id serial primary key,
+    token text not null
+);
